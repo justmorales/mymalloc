9b45cd9aa69dc7b35f63b120f52992a727138a1f
jusjusten <justindejesus323@gmail.com>
2024-10-09 21:33:50 -0400

    with comments and the right stderr lines

--- mymalloc.c
+++ mymalloc.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "mymalloc.h"
 
+#define DEBUG 0
 
 // Initialize the "heap"
 #define MEMLENGTH 4096
@@ -21,27 +22,56 @@ static union {
 static int initialized;
 
 static void leak_checker() {
-    int count = 0, bytes = 0;
+    int count = 0;
+    int bytes = 0;
     metadata* node = (metadata*)heap.bytes;
 
-    // Traverse through heap
-    while (node->allocated) {
-        count++;
-        bytes += node->size;
-        node = node + node->size + HEADERSIZE;
+    while (node < (metadata*)(heap.bytes + MEMLENGTH)) {
+        if (DEBUG) printf("checking node %p for leak...\n", node);
+        if (node->allocated) {
+            count = count + 1;
+            if (DEBUG) printf("...adding %d to bytes from...%p\n", HEADERSIZE+node->size, node);
+            bytes += HEADERSIZE + node->size;
+        }
+        node = (metadata*)((char*)node + HEADERSIZE + node->size);
     }
 
-    printf("\nmymalloc: %d bytes leaked in %d objects\n", bytes, count);
+    printf("mymalloc: %d bytes leaked in %d objects\n", bytes, count);
 }
 
 static void initialize_heap() {
     // Create initial header and set size to MEMLENGTH
     metadata* head = (metadata*)heap.bytes;
-    printf("INITIAL HEADER: %p\n", head);
     head->size = MEMLENGTH - HEADERSIZE;
     head->allocated = 0;
     initialized = 1;
     atexit(leak_checker);
+}
+
+void merge() {
+    //grab the block we are working with
+    metadata *curr = (metadata *)heap.bytes;
+
+    // while inside the heap, we iterate to get to the one we are working with and check if we have block free and the back.
+ 	while (curr < (metadata*)(heap.bytes + MEMLENGTH)) {
+        metadata *next = (metadata*)((char*)curr + curr->size + HEADERSIZE);
+        if (!curr->allocated) {
+            // Check if next header is not end of memory and is unallocated
+            if (next != (metadata*)(heap.bytes + MEMLENGTH) && next->allocated == 0 && next->size != 0) {
+                curr->size += next->size + HEADERSIZE;
+                merge();
+                if (DEBUG) leak_checker();
+            } else {
+                // Check if next header is still in memory
+                if (next < (metadata*)(heap.bytes + MEMLENGTH)) {
+                    curr = next;
+                } else 
+                    return;
+            }
+        } else
+            curr = next;
+        return;
+    }
 }
 
 /**
@@ -52,7 +82,7 @@ static void initialize_heap() {
 void *mymalloc(size_t size, char *file, int line){
     // Check if number of bytes requested is valid
     if (size <= 0 || size >= MEMLENGTH - HEADERSIZE) {
-        printf("mymalloc: Size of bytes requested is too small or too large (%s:%d)\n", file, line);
+        fprintf(stderr, "malloc: Unable to allocate 1234 bytes (mymalloc.c:1000)\n");
         return NULL;
     }
 
@@ -62,29 +92,30 @@ void *mymalloc(size_t size, char *file, int line){
 
     metadata* curr = (metadata*)heap.bytes;
 
+    //keep going until we check all of heap
     while (curr < (metadata*)(heap.bytes + MEMLENGTH)) {
-        leak_checker();
-        if (!curr->allocated) {
-            printf("%p is not allocated\n", curr);
+        if (DEBUG) printf("size: %d vs %d\n", curr->size + HEADERSIZE, size + HEADERSIZE);
+        // if it is not allocated and the size requested is smaller than the amt we have open
+        if (!curr->allocated && curr->size >= size) {
+            if (DEBUG) printf("\n%p is not allocated\n", curr);
             // If chunk is big enough to split into 2
-            if (curr->size - size >= HEADERSIZE * 2) {
+            if (curr->size > size) {
+                if (DEBUG) printf("Current chunk size (presplit): %d\n", curr->size);
                 metadata* new = (metadata*)((char*)curr + HEADERSIZE + size);
-                //printf("\nWHAT: %d\n", curr->size);
-                new->size = curr->size - size;
+                new->size = curr->size - size - HEADERSIZE;
                 new->allocated = 0;
 
                 curr->size = size;
-                printf("\tCurrent chunk located %p of size %d\n", curr, curr->size + 8);
-                printf("\tNew chunk located %p of size %d\n", new, new->size + 8);
+                if (DEBUG) printf("\tCurrent chunk located %p of size %d\n", curr, curr->size + 8);
+                if (DEBUG) printf("\tNew chunk located %p of size %d\n", new, new->size + 8);
             }
             // Return pointer to payload by adding 1 * sizeof(metadata)
-            printf("\tcurr addy: %p\n", curr);
             curr->allocated = 1;
             return (void*)(curr+1);
         }
-        printf("%p is allocated by a chunk of size %d\n", curr, curr->size + 8);
+        if (DEBUG) printf("%p is allocated by a chunk of size %d\n", curr, curr->size + 8);
         curr = (metadata*)((char*)curr + curr->size + HEADERSIZE);
-    }
+    }   
 
     printf("mymalloc: Insufficient memory for requested bytes (%s:%d)\n", file, line);
     return NULL;
@@ -97,82 +128,64 @@ void *mymalloc(size_t size, char *file, int line){
  */
 void myfree(void *ptr, char *file, int line) {
 	//header size invluded in payload size
-	char *pointer = (char *)ptr - HEADERSIZE;
+	ptr = (void*)((char*)ptr - HEADERSIZE);
 
-    //printf("%p\n",pointer);
-	//check if the pointer exists
-	if(pointer == NULL){
-		fprintf(stderr, "%s:%d:free: Can't free NULL\n", file, line);
+    //check if ptr is even available
+	if(ptr == NULL){
+		fprintf(stderr, "free: Inappropriate pointer (mymalloc.c:1000)\n");
 		return;
 	}
-
 
 	//check if pointer is inside the memory
-
-	if(pointer < (char*)heap.bytes || pointer >= (char*)(heap.bytes + MEMLENGTH)){
-		fprintf(stderr, "%s:%d:free: Pointer out of memory[]\n", file, line);
+	if((metadata*)ptr < (metadata*)heap.bytes || (metadata*)ptr >= (metadata*)((char*)(heap.bytes + MEMLENGTH))){
+		fprintf(stderr, "free: Inappropriate pointer (mymalloc.c:1000)\n");
 		return;
 	}
 
-	//finding the block we are working with
-	//tbh maybe i should make a function to do this so that it can return null if we dont find the value we are looking for
-	// char *iterate = (metadata *)(heap.bytes);
-	// struct metadata *meta = (struct metadata *)iterate;
-	
 	metadata *meta = (metadata *)heap.bytes;
-    //printf("meta is : %p\n\n",meta);
 
+    // check meta is inside the heap
 	while(meta < (metadata*)(heap.bytes + MEMLENGTH)){
-        //printf("meta->size : %d\n",meta->size);
-
-		if(meta == (metadata*)pointer){
-			break;
-		}
+		if (meta == ptr && meta->allocated){
+            // if (DEBUG) printf("meta %p == requested %p\n", meta, ptr);
+			// break;
+            if (DEBUG) printf("\tfreeing up %p\n", meta);
+            meta->allocated = 0;
+            merge();
+            return;
+		} else {
+            if (meta == ptr)
+		        fprintf(stderr, "free: Inappropriate pointer (mymalloc.c:1000)\n");
+        }
 		//move to the next block
 		meta = (metadata*)((char*)meta + meta->size + HEADERSIZE);
-		
 	}
+    // if (DEBUG) printf("\tfreeing up %p\n", meta);
+	// meta->allocated = 0;
 
-    // printf("meta is : %p\n",meta);
-    // printf("meta->allocated is: %d\n", meta->allocated);
-    // printf("meta->size is: %d\n", meta->size);
+        // modularize this into merge()
+    
+    // metadata *prev = NULL;
+    // metadata *curr = (metadata *)heap.bytes;
 
-	//we have the block we are working with and checking if it is malloced or not
-	if(meta->allocated == 0){
-		fprintf(stderr, "%s:%d:free Pointer not malloc-ed\n", file, line);
-	    return;
-	}
-	//printf("%d\n", meta->allocated);
-	meta->allocated = 0;
-	//printf("%d\n", meta->allocated);
-
-
-	//merge();
-
-	//merge time
-
-	
-	// struct metadata *current = (struct metadata *)iterate;
-	// struct metadata *previous = NULL;
-    metadata *prev = NULL;
-    metadata *curr = (metadata *)heap.bytes;
-    //metadata *next = curr + sizeof(metadata) + curr->size;
-
-
- 	while (curr < (metadata*)(heap.bytes + MEMLENGTH)) {
-        if (prev && prev->allocated == 0 && curr->allocated == 0) {
-            prev->size += sizeof(metadata) + curr->size;
-        }else {
-            prev = curr;
-        }
-        // if(next->allocated == 0 && curr->allocated == 0){
-        //     curr->size += sizeof(metadata) + next->size;
-        // }
-        curr += sizeof(metadata) + curr->size;
-        //next += sizeof(metadata) + next->size;
-    }
-
-
-
-
+ 	// while (curr < (metadata*)(heap.bytes + MEMLENGTH)) {
+    //     metadata *next = (metadata*)((char*)curr + curr->size + HEADERSIZE);
+    //     if (!curr->allocated) {
+    //         // Check if next header is not end of memory and is unallocated
+    //         printf("Current header size: %d\n", curr->size);
+    //         printf("Next header size: %d\n", next->size);
+    //         if (next != (metadata*)(heap.bytes + MEMLENGTH) && next->allocated == 0) {
+    //             printf("COALESCE");
+    //             curr->size += next->size + HEADERSIZE;
+    //             leak_checker();
+    //         } else {
+    //             if (next < (metadata*)(heap.bytes + MEMLENGTH)) {
+    //                 curr = next;
+    //             } else 
+    //                 return;
+    //         }
+    //     } else
+    //         curr = next;
+    //     return;
+    // }
 }
